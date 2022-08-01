48ca20199fa697c15202027d25bea917edadbb9d
omer <omerme13@gmail.com>
2022-08-01 22:53:10 +0300

    add fade animation to card

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './Card.scss';
+import React, { useEffect, useState } from "react";
+import "./Card.scss";
 
-const Card = ({children}) => {
-  return (<div className="card">
-    {children}
-  </div>);
-}
- 
+const Card = ({ children }) => {
+    const [opacity, setOpacity] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setOpacity(1);
+        },100)
+    }, []);
+
+    return (
+        <div className="card" style={{ opacity }}>
+            {children}
+        </div>
+    );
+};
+
 export default Card;
