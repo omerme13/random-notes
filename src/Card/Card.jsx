8ac27f938c8100f448f74c49285c0dd0ceb92c51
omer <omerme13@gmail.com>
2022-10-14 19:09:10 +0300

    fix: use keyframe instead of usefade animation

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import useFadeStyle from "../hooks/useFadeStyle";
 import "./Card.scss";
 
 const Card = ({ children }) => {
-    const fadeStyle = useFadeStyle({ delay: 100, duration: 1 });
 
     return (
-        <div className="card" style={fadeStyle}>
+        <div className="card">
             {children}
         </div>
     );
